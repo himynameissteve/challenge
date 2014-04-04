dcddec33401135f71e91aaa80c991dbde36422c5
Stefan Antal <stefanantal@gmail.com>
2014-04-04 22:40:29 +0200

    Minor output changes

--- 2DArray/src/com/array/main/Main.java
+++ 2DArray/src/com/array/main/Main.java
@@ -6,14 +6,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		Field field = new Field(3, 3);
+
 		field.setupField();
 		field.printField();
+
+		System.out.println();
+
 		field.rotateCw();
 		field.printField();
+
+		System.out.println();
+
 		field.rotateCw();
 		field.printField();
+
+		System.out.println();
+
 		field.rotateCw();
 		field.printField();
+
+		System.out.println();
+
 		field.rotateCw();
 		field.printField();
 	}
